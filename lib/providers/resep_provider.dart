09116baa41f1a74a9db5b6867ca64048b31429a3
faisalluthfi <faisalluthfi02@gmail.com>
2022-06-24 21:22:54 +0700

    third commit

--- lib/providers/resep_provider.dart
+++ lib/providers/resep_provider.dart
@@ -29,27 +29,9 @@ class ResepProvider extends ChangeNotifier{
 }
 
 class ResepDetailProvider with ChangeNotifier{
-  // String? id;
-  // void setId(String? id2){
-  //   id = id2;
-  //   // print(id2);
-  // }
-
-  // String? get _getId {
-  //   return id;
-  // }
-  // ResepDetailProvider({this.id});
 
   final ResepService _ResepService = ResepService();
   DetailResepModel detailResepModel = DetailResepModel();
-
-
-  showDetailResep (String? id) async {
-    // detailResepModel = await _ResepService.getDetailResep(key: id);
-    print('id $id');
-    notifyListeners();
-
-  }
 
 }
 
